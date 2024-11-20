41cbe5348bc77d3c8634b486747c8705f6a80af4
Kyla Zahra Winnetou <kylazahraw@gmail.com>
2024-11-20 11:14:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mealgo/SplashscActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mealgo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashscActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashsc)
+    }
+}
