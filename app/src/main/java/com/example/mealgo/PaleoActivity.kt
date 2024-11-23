a12f4205a9a47318f8bcb78bb16182bad3f60ed0
Kyla Zahra Winnetou <kylazahraw@gmail.com>
2024-11-23 14:54:17 +0700

    Commit Kedua

---
+++ app/src/main/java/com/example/mealgo/PaleoActivity.kt
@@ -0,0 +1,20 @@
+package com.example.mealgo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PaleoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_paleo)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
